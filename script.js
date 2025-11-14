a23b24fe4c25ff8f41aea1e36b1c3bacb5e6eb27
Alison Debaille <adebaille@gmail.com>
2025-11-14 12:38:18 +0100

    finish

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+function createElement(element, content, tag){
+    descriptionElement = document.createElement (element);
+    descriptionElement.textContent = content;
+    document.querySelector(tag).appendChild(descriptionElement);
+}
+
 async function fetchOffers() {
     const response = await fetch("https://corsproxy.io/?url=https://codepassport.dev/api/offers");
     const offers = await response.json();
-    console.log(offers)
+    
+    offers.forEach(element => {
+        createElement("h2", element.titre, "body");
+        createElement("p", element.technologie, "body");
+        createElement("p", element.description, "body");
+    });
 }
 fetchOffers();
-
-const titleElement = document.createElement ('h2');
-const descriptionElement = document.createElement ('p');
